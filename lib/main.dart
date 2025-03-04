71b5cdaddf247b31776c6f026015e004e6e087f7
ANDRES_MIGUEL_RIVERA_FRANCISCO <a.22308051281295@cbtis128.edu.mx>
2025-03-04 16:45:25 +0000

    Practica 10-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyContainersApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyContainersApp extends StatelessWidget {
+  const MyContainersApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ejemplo Container | Andres Rivera 1295'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        body: Stack(
+          children: <Widget>[
+            // Fondo con dos colores usando LinearGradient
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.blue, Colors.white],
+                  stops: [0.65, 0.65], // <-- Aquí se define la división
+                ),
+              ),
+            ),
+            // Contenedor blanco en la parte inferior
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 80),
+                child: Container(
+                  width: 320,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Center(),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
